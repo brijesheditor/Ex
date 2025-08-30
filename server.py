8aa6bce5b428c0044665f5458a0942469a84c926
sujalchavdaa <sujalchavda500@gmail.com>
2025-08-30 12:04:58 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
 from flask import Flask
 import os
+import subprocess
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
+    # Bot ko background me start karo
+    try:
+        # Ye command Extractor/__main__.py run karega
+        subprocess.Popen(["python", "-m", "Extractor"])
+    except Exception as e:
+        print("Bot already running or failed:", e)
+
     return "Bot is running ✅"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render automatically port assign karega
+    port = int(os.environ.get("PORT", 10000))  # Render automatically PORT assign karega
     app.run(host="0.0.0.0", port=port)
