86efa09ff95b3fa31437ec9e65e9540109628f3c
sujalchavdaa <sujalchavda500@gmail.com>
2025-09-02 23:05:51 +0530

    Update start.py

--- Extractor/modules/start.py
+++ Extractor/modules/start.py
@@ -13,7 +13,7 @@ from Extractor import app
 from config import OWNER_ID, CHANNEL_ID
 from Extractor.core import script
 from Extractor.core.func import subscribe, chk_user
-from Extractor.modules.cdsfree import handle_cds_journey, handle_cds_callback, handle_batch_message
+#from Extractor.modules.cdsfree import handle_cds_journey, handle_cds_callback, handle_batch_message
 from Extractor.modules.appex_v1 import api_v1
 from Extractor.modules.appex_v2 import appex_v2_txt
 from Extractor.modules.appex_v3 import appex_v5_txt
