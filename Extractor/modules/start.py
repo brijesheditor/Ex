4c0b41d7b3f6e0deda54a733f2b9d94b599a3c70
sujalchavdaa <sujalchavda500@gmail.com>
2025-09-02 18:25:25 +0530

    Update start.py

--- Extractor/modules/start.py
+++ Extractor/modules/start.py
@@ -295,7 +295,6 @@ async def start(_, message):
             "🚀 Initializing System...\n【■■■■□□□□□□□□】 25%",
             "🚀 Initializing System...\n【■■■■■■■■□□□□】 50%",
             "🚀 Initializing System...\n【■■■■■■■■■■■■】 75%",
-            "🚀 Initializing System...\n【■■■■■■■■■■■■■■■■】 100%",
         ]
 
         for frame in loading_frames:
